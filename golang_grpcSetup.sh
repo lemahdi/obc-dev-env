f41ddbff84257ba2390b15feef651c9666587657
srderson <sranderson@gmail.com>
2015-09-23 17:09:19 -0400

    Compile openchain-peer proto files

--- golang_grpcSetup.sh
+++ golang_grpcSetup.sh
@@ -26,7 +26,7 @@ set -x
 
 # ----------------------------------------------------------------
 # NOTE: For instructions, see https://github.com/google/protobuf
-# 
+#
 # ----------------------------------------------------------------
 
 # First install protoc
@@ -38,9 +38,9 @@ sudo apt-get install -y unzip
 sudo apt-get install -y autoconf
 sudo apt-get install -y build-essential libtool
 ./autogen.sh
-# NOTE: By default, the package will be installed to /usr/local. However, on many platforms, /usr/local/lib is not part of LD_LIBRARY_PATH. 
-# You can add it, but it may be easier to just install to /usr instead.  
-# 
+# NOTE: By default, the package will be installed to /usr/local. However, on many platforms, /usr/local/lib is not part of LD_LIBRARY_PATH.
+# You can add it, but it may be easier to just install to /usr instead.
+#
 # To do this, invoke configure as follows:
 #
 # ./configure --prefix=/usr
@@ -53,6 +53,9 @@ make check
 sudo make install
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-# Install the protoc Go plugin.  
+# Install the protoc Go plugin.
 go get -a github.com/golang/protobuf/protoc-gen-go
 
+# Compile proto files required by openchain-peer
+protoc --go_out=plugins=grpc:$GOPATH/src /usr/include/google/protobuf/timestamp.proto
+protoc --go_out=plugins=grpc:$GOPATH/src /usr/include/google/protobuf/empty.proto
